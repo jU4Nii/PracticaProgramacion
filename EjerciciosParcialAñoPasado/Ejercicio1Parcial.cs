727d6fe16a945d7d9ef6e0de1baab5ed41bb1744
jU4Nii <juanibonora2004@gmail.com>
2025-05-13 18:01:22 -0300

    Práctica

---
+++ EjerciciosParcialAñoPasado/Ejercicio1Parcial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace EjerciciosParcialAñoPasado
+{
+    internal class Ejercicio1Parcial
+    {
+
+        public static void Ejecutar()
+        {
+            List<int> lista = new List<int>();
+
+            Console.WriteLine("Ingresar números para filtrar los impares:");
+            
+            string texto = Console.ReadLine();
+
+            int num;
+
+            bool conversion = int.TryParse(texto, out num);
+
+            while(conversion == false)
+            {
+                Console.WriteLine("Valor incorrecto, ingresar de vuelta:");
+
+                texto = Console.ReadLine();
+
+                conversion = int.TryParse(texto, out num);
+            }
+
+            while (num != 0)
+            {
+                lista.Add(num);
+
+                texto = Console.ReadLine();
+
+                conversion = int.TryParse(texto, out num);
+
+                while (conversion == false)
+                {
+                    Console.WriteLine("Valor incorrecto, ingresar de vuelta:");
+
+                    texto = Console.ReadLine();
+
+                    conversion = int.TryParse(texto, out num);
+                }
+            }
+
+            List<int> listaImpares = lista.Where(n => n % 2 != 0).OrderByDescending(x => x).ToList();
+
+            foreach(int n in listaImpares)
+            {
+
+                Console.WriteLine(n);
+
+            }
+
+
+
+
+
+        }
+
+    }
+}
---
+++ EjerciciosParcialAñoPasado/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EjerciciosParcialAñoPasado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicio1Parcial.Ejecutar();
+        }
+    }
+}
