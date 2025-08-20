3bb2aa173e4c0ee60160f72a7b2324423e31f1ae
jU4Nii <juanibonora2004@gmail.com>
2025-08-20 18:50:33 -0300

    EjercicioGestorDeViajes

---
+++ EjercicioSistemaGestionViajes/BibliotecaDeClasesGestionDeViajes/Viaje.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClasesGestionDeViajes
+{
+    public class Viaje
+    {
+
+        public int Distancia { get; set; }
+
+        public int CargaTransportada { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+    }
+}
